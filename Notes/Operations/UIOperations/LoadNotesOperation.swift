4b310e95620439f65bca01e32a20ecd493b4ac10
Igor Podolskiy <Delusionhs@mail.ru>
2019-07-30 22:12:15 +1100

    loadNotesoperation

--- Notes/Operations/UIOperations/LoadNotesOperation.swift
+++ Notes/Operations/UIOperations/LoadNotesOperation.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+class LoadNoteOperation: AsyncOperation {
+    private let notebook: FileNotebook
+    private let loadToDb: LoadNotesDBOperation
+    private var loadToBackend: LoadNotesBackendOperation?
+    
+    private(set) var result: Bool? = false
+    
+    init(note: Note,
+         notebook: FileNotebook,
+         backendQueue: OperationQueue,
+         dbQueue: OperationQueue) {
+        self.notebook = notebook
+        
+        loadToDb = LoadNotesDBOperation(notebook: notebook)
+        
+        super.init()
+        
+        loadToDb.completionBlock = {
+            let loadToBackend = LoadNotesBackendOperation(notes: notebook.notes)
+            self.loadToBackend = loadToBackend
+            self.addDependency(loadToBackend)
+            backendQueue.addOperation(loadToBackend)
+        }
+        
+        addDependency(loadToDb)
+        dbQueue.addOperation(loadToDb)
+    }
+    
+    override func main() {
+        switch loadToBackend!.result! {
+        case .success:
+            result = true
+        case .failure:
+            result = false
+        }
+        finish()
+    }
+}
+
