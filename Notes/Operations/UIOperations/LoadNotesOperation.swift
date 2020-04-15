98bc316299641c79fa4c05eca286ae10cf311301
Igor Podolsiy <delusionhs@mail.ru>
2020-04-15 21:48:36 +1100

    add UI operations

---
+++ Notes/Operations/UIOperations/LoadNotesOperation.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+class LoadNotesOperation: AsyncOperation {
+    
+  private let loadFromDb: LoadNotesDBOperation
+  private let dbQueue: OperationQueue
+  
+  private(set) var result: Bool? = false
+  
+  init(notebook: FileNotebook,
+       backendQueue: OperationQueue,
+       dbQueue: OperationQueue) {
+    
+    loadFromDb = LoadNotesDBOperation(notebook: notebook)
+    self.dbQueue = dbQueue
+    
+    super.init()
+    loadFromDb.completionBlock = {
+      let loadFromBackend = LoadNotesBackendOperation()
+      loadFromBackend.completionBlock = {
+        switch loadFromBackend.result! {
+        case .success:
+          self.result = true
+        case .failure:
+          self.result = false
+        }
+        self.finish()
+      }
+      backendQueue.addOperation(loadFromBackend)
+    }
+  }
+  
+  override func main() {
+    dbQueue.addOperation(loadFromDb)
+  }
+}
---
+++ Notes/Operations/UIOperations/RemoveNoteOperation.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+class RemoveNoteOperation: AsyncOperation {
+    
+  private let removeFromDb: RemoveNoteDBOperation
+  private let dbQueue: OperationQueue
+  
+  private(set) var result: Bool? = false
+  
+  init(note: Note,
+       notebook: FileNotebook,
+       backendQueue: OperationQueue,
+       dbQueue: OperationQueue) {
+    
+    removeFromDb = RemoveNoteDBOperation(note: note, notebook: notebook)
+    self.dbQueue = dbQueue
+    
+    super.init()
+    
+    removeFromDb.completionBlock = {
+      let saveToBackend = SaveNotesBackendOperation(notes: notebook.notes)
+      saveToBackend.completionBlock = {
+        switch saveToBackend.result! {
+        case .success:
+          self.result = true
+        case .failure:
+          self.result = false
+        }
+        self.finish()
+      }
+      backendQueue.addOperation(saveToBackend)
+    }
+  }
+  
+  override func main() {
+    dbQueue.addOperation(removeFromDb)
+  }
+}
