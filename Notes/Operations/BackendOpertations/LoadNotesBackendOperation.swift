4f9d0d83cceba058c217e496993f568378ef0b86
Igor Podolsiy <delusionhs@mail.ru>
2020-04-24 22:52:33 +1100

    BAD solution with pyramid of doom

--- Notes/Operations/BackendOpertations/LoadNotesBackendOperation.swift
+++ Notes/Operations/BackendOpertations/LoadNotesBackendOperation.swift
@@ -14,54 +14,57 @@ class LoadNotesBackendOperation: BaseBackendOperation {
   
     override func main() {
         
-        guard let requestGistList = getRequestWithToken(urlString: "https://api.github.com/gists") else {
-            self.result = .failure(.dataFailure)
-            finish()
-            return }
-        
-        URLSession.shared.dataTask(with: requestGistList) { (data, response, error) in
-            if let data = data {
-                guard let gists =  try? JSONDecoder().decode([Gist].self, from: data) else  { return }
-                for gist in gists {
-                    for (key,_) in gist.files {
-                        if key == "ios-course-notes-db" {
-                            guard let id = gist.id else { return }
-            }}}}}.resume()
-        
-
-        guard let request = getRequestWithToken(urlString: "https://api.github.com/gists/828262049b6e86cf5f97bf8ef504e2a2") else {
+        guard let requestGistID = getRequestWithToken(urlString: "https://api.github.com/gists") else {
             self.result = .failure(.dataFailure)
             finish()
             return }
     
+        URLSession.shared.dataTask(with: requestGistID ) { (data, response, error) in
+            if let data = data {
+                guard let gists =  try? JSONDecoder().decode([Gist].self, from: data) else  {
+                    self.result = .failure(.dataFailure)
+                    self.finish()
+                    return }
+                for gist in gists {
+                    for (key,_) in gist.files {
+                        if key == "ios-course-notes-db" {
+                            guard let id = gist.id else { return }
+                            //DATA TASK TWO START
+                            guard let request = self.getRequestWithToken(urlString: "https://api.github.com/gists/" + id) else {
+                                    self.result = .failure(.dataFailure)
+                                    self.finish()
+                                    return }
+                                URLSession.shared.dataTask(with: request) { (data, response, error) in
+                                    if let response = response as? HTTPURLResponse {
+                                        switch response.statusCode {
+                                        case 200..<300:
+                                            if let data = data {
+                                                guard let gist =  try? JSONDecoder().decode(Gist.self, from: data) else {
+                                                    print("Can't parse")
+                                                    return
+                                                    }
+                                                if let content = gist.files[self.filename]?.content {
+                                                    let data = Data()
+                                                    guard let notes = try? JSONDecoder().decode([NoteCodable].self, from: content.data(using: .utf8) ?? data) else {
+                                                        print("Can't parse")
+                                                        return
+                                                    }
+                                                    self.notebook.loadNotesFromCodable(notes: notes)
+                                                }
+                                            }
+                                            self.result = .success
+                                        default:
+                                            self.result = .failure(.unreachable)
+                                        }
+                                    }
+                                    self.finish()
+                                }.resume()
+            }}}}}.resume()
         
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let response = response as? HTTPURLResponse {
-                switch response.statusCode {
-                case 200..<300:
-                    if let data = data {
-                        guard let gist =  try? JSONDecoder().decode(Gist.self, from: data) else {
-                            print("Can't parse")
-                            return
-                            }
-                        if let content = gist.files[self.filename]?.content {
-                            let data = Data()
-                            guard let notes = try? JSONDecoder().decode([NoteCodable].self, from: content.data(using: .utf8) ?? data) else {
-                                print("Can't parse")
-                                return
-                            }
-                            self.notebook.loadNotesFromCodable(notes: notes)
-                        }
-                    }
-                    self.result = .success
-                default:
-                    self.result = .failure(.unreachable)
-                }
-            }
 
-            self.finish()
-        }.resume()
+
     }
+
     
     func getRequestWithToken(urlString: String) -> URLRequest? {
         var components = URLComponents(string: urlString)
