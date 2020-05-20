e44f958674531ca35b825f8f933f40dc997b347a
Igor Podolsiy <delusionhs@mail.ru>
2020-05-20 16:37:39 +1100

    savedboperationa functional

--- Notes/Operations/DBOperations/SaveNoteDBOperation.swift
+++ Notes/Operations/DBOperations/SaveNoteDBOperation.swift
@@ -14,17 +14,51 @@ class SaveNoteDBOperation: BaseDBOperation {
     }
 
     override func main() {
-        
-        let noteEntity = NoteEntity(context: backgroundContext)
-        noteEntity.uid = note.uid
-        noteEntity.title = note.title
-        noteEntity.content = note.content
-        noteEntity.importance = note.importance.rawValue
-        noteEntity.destructionDate = note.selfDestructionDate
-        noteEntity.colorRed = Float(Color(uiColor: note.color).red)
-        noteEntity.colorGreen = Float(Color(uiColor: note.color).green)
-        noteEntity.colorBlue = Float(Color(uiColor: note.color).blue)
-        noteEntity.colorAlpha = Float(Color(uiColor: note.color).alpha)
+        let fetchRequest = NSFetchRequest<NoteEntity>(entityName: "NoteEntity")
+        fetchRequest.predicate = NSPredicate(format: "uid == %@", note.uid)
+        do {
+            let fetchedObjects = try backgroundContext.fetch(fetchRequest)
+            if fetchedObjects.isEmpty {
+                let noteEntity = NoteEntity(context: backgroundContext)
+                noteEntity.uid = note.uid
+                noteEntity.title = note.title
+                noteEntity.content = note.content
+                noteEntity.importance = note.importance.rawValue
+                noteEntity.destructionDate = note.selfDestructionDate
+                noteEntity.colorRed = Float(Color(uiColor: note.color).red)
+                noteEntity.colorGreen = Float(Color(uiColor: note.color).green)
+                noteEntity.colorBlue = Float(Color(uiColor: note.color).blue)
+                noteEntity.colorAlpha = Float(Color(uiColor: note.color).alpha)
+                
+                backgroundContext.performAndWait {
+                    do {
+                        try self.backgroundContext.save()
+                    } catch {
+                        print(error)
+                    }
+                }
+            } else {
+                fetchedObjects[0].title = note.title
+                fetchedObjects[0].content = note.content
+                fetchedObjects[0].importance = note.importance.rawValue
+                fetchedObjects[0].destructionDate = note.selfDestructionDate
+                fetchedObjects[0].colorRed = Float(Color(uiColor: note.color).red)
+                fetchedObjects[0].colorGreen = Float(Color(uiColor: note.color).green)
+                fetchedObjects[0].colorBlue = Float(Color(uiColor: note.color).blue)
+                fetchedObjects[0].colorAlpha = Float(Color(uiColor: note.color).alpha)
+                
+                backgroundContext.performAndWait {
+                    do {
+                        try self.backgroundContext.save()
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            
+        } catch {
+            print("Error")
+        }
         
         notebook.add(self.note)
         
