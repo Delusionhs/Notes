52bdaa7b5a0c7a7348baee4a6fc36d5563889d02
Igor Podolsiy <delusionhs@mail.ru>
2020-05-23 18:04:43 +1100

    add network layer

---
+++ Notes/NetworkLayer.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkLayer.swift
+//  Notes
+//
+//  Created by Igor Podolskiy on 23.05.2020.
+//  Copyright © 2020 Igor Podolskiy. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+class NetworkLayer {
+    static let instance = NetworkLayer()
+    
+    func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+}
